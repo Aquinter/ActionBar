b3b554f4fe97930d6b6639385d3dd90bfeaebb80
Xavier <xavier@dejager.be>
2014-03-29 10:51:34 +0100

    -added DeviceId class, implementation partialy made for IOS

---
+++ ActionBar/Resources/Klasses/GUI/SimpleDialogManager_iOS.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace IngHackaton //Moet maar aangepast worden voor het project
+//{
+//	public class SimpleDialogManager_iOS
+//	{
+//		//public class SimpleDialogManager_iOS(title message yes no cancel)
+//		{
+//
+//		}
+//
+//		//public bool Show()
+//		{
+//			new StringElement ("Click me",() => { new UIAlertView("Tapped", "String Element Tapped", null, "ok", null).Show(); })
+//		}
+//	}
+//}
