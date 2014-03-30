defaa54583906020bcc424a7cf4ea0f28fabc817
Xavier <xavier@dejager.be>
2014-03-30 09:19:01 +0200

    - solved git merge errors in Perimeter class

--- ActionBar/Resources/Klasses/Perimeter.cs
+++ ActionBar/Resources/Klasses/Perimeter.cs
@@ -8,8 +8,8 @@ namespace IngHackaton //Moet maar aangepast worden voor het project
 	{
 		Coordinate epicenter;
 		double shortRange;
-		double longRange;
-  		public string name { get; set;}
+  		double longRange;
+  public string name { get; set;}
 
 		public Perimeter(Coordinate epicenter)
 		{
@@ -25,15 +25,13 @@ namespace IngHackaton //Moet maar aangepast worden voor het project
 		}
 		public int getZoneRating(Coordinate coordinate) // 0 = home, 1 = public, 2 = forbidden, 4 = error
 		{
-			double distanceBetween = this.epicenter.distanceBetween (coordinate);
-   if (distanceBetween >= 0 && distanceBetween <= shortRange)
-			double dist = Math.Abs(this.epicenter.distanceBetween (coordinate));
+		double dist = Math.Abs(this.epicenter.distanceBetween (coordinate));
 
 			if (dist >= 0.0 && dist <= this.shortRange)
 			{
 				return 0;
 			} 
-			else if (dist > this.shortRange && dist <= this.longRange)
+   			else if (dist > this.shortRange && dist <= this.longRange)
 			{
 				return 1;
 			} 
